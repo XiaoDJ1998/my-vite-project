024521eafdbcaf757bfbf2e26216f782daf08e76
unknown <1132030982@qq.com>
2024-10-02 15:51:19 +0800

    second commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data(){
+    return {
+      name:"董佳"
+    }
+  },
+
+  methods: {
+    clickName(){
+      this.name = '杨琴';
+    }
+  }
+}
+</script>
+
+<template>
+  <span>{{ name }}</span>
+  <button @click="clickName"></button>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
